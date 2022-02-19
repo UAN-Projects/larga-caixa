632c07009d4fe09096a713de92a73d6c4955d56b
Edvaldo Valente <edvaldo.valente@ideiasdinamicas.com>
2022-02-19 12:03:36 +0100

    get files with access

--- server.php
+++ server.php
@@ -8,21 +8,57 @@
     function files($token) {
         $conn = conexao();
         $stmt = $conn->prepare(
-          "SELECT ficheiros.descricao, ficheiros.preco, ficheiros.ficheiro, ficheiros.created_at
-          from ficheiros join users on ficheiros.user_id = users.id
-            WHERE users.token = :token");
+          "SELECT ficheiros.id, ficheiros.descricao, ficheiros.preco, ficheiros.ficheiro, ficheiros.created_at
+          from ficheiros 
+          join users on ficheiros.user_id = users.id
+          WHERE users.token = :token");
         $stmt->execute(array('token' => $token));
         $ficheiros = $stmt->fetchAll();
-        $return = array();
-        
+
+        $stmt2 = $conn->prepare(
+            "SELECT ficheiros.id, ficheiros.descricao, ficheiros.preco, ficheiros.ficheiro, ficheiros.created_at
+            from ficheiros 
+            join users on ficheiros.user_id = users.id
+            join users_ficheiros on ficheiros.id = users_ficheiros.ficheiro_id
+            WHERE users.token = :token");
+          $stmt2->execute(array('token' => $token));
+          $ficheiros2 = $stmt2->fetchAll();
+
+        $files = [];
         foreach ($ficheiros as $ficheiro) {
-            $return = array_merge($return, 
-                array(
-                    array( 
+            foreach ($ficheiros2 as $ficheiro2) {
+                if($ficheiro2['id'] != $ficheiro['id'] ) {
+                    array_push($files,array(
                         'descricao' => $ficheiro['descricao'], 
                         'preco' => $ficheiro['preco'],
                         'ficheiro' => "http://{$_SERVER['HTTP_HOST']}/uploads/ficheiros/".$ficheiro['ficheiro'],
+                        'link' => "http://{$_SERVER['HTTP_HOST']}/ficheiro/".$ficheiro['id'],
                         'data' => $ficheiro['created_at'],
+                    ));
+                }
+            }   
+        }
+
+        foreach ($ficheiros2 as $ficheiro2) {
+            array_push($files,array(
+                'descricao' => $ficheiro2['descricao'], 
+                'preco' => $ficheiro2['preco'],
+                'ficheiro' => "http://{$_SERVER['HTTP_HOST']}/uploads/ficheiros/".$ficheiro2['ficheiro'],
+                'link' => "http://{$_SERVER['HTTP_HOST']}/ficheiro/".$ficheiro2['id'],
+                'data' => $ficheiro2['created_at'],
+            ));
+        }
+        
+        $return = array();
+        foreach ($files as $file) {
+            $return = array_merge($return, 
+                array(
+                    array( 
+                        'descricao' => $file['descricao'], 
+                        'preco' => $file['preco'],
+                        'ficheiro' => $file['ficheiro'],
+                        'link' => $file['link'],
+                        'data' => $file['data'],
                     ),
                 )
             );
@@ -59,6 +95,7 @@
             'descricao' => array('name' => 'descricao', 'type' => 'xsd:string'),
             'preco' => array('name' => 'preco', 'type' => 'xsd:string'),
             'ficheiro' => array('name' => 'file', 'type' => 'xsd:string'),
+            'link' => array('name' => 'file', 'type' => 'xsd:string'),
             'data' => array('name' => 'data', 'type' => 'xsd:string')
         )
     );
